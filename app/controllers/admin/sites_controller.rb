f2557ff716560dacf40068f4347f0eb918f2e01f
Jim Mulholland <jim@jim-mb-pro.local>
2009-01-23 16:05:28 -0600

    Filtered sites by site; Set the site parent_id to the current site_id on create;

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,3 +1,12 @@
 class Admin::SitesController< Admin::BaseController
- resource_controller
+  resource_controller
+ 
+  create.before do
+    @site.parent_id = @current_site.id
+  end
+ 
+  private
+  def collection
+    @collection ||= @site.self_and_children
+  end
 end
